0384490961d0099ac1549915a3403d5cc2c727fd
Prince Odia <princeodia@gmail.com>
2022-01-28 22:51:11 -1200

    Filter and limit data

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ async function main() {
     const getData = await circulationRepo.get();
     assert.equal(data.length, getData.length);
 
+    const filterData = await circulationRepo.get({Newspaper: getData[4].Newspaper}); 
+    assert.deepEqual(filterData[0], getData[4]);
+
+    const limitData = await circulationRepo.get({}, 3);
+    assert.equal(limitData.length, 3);
+
   } catch (error) {
         console.log(error);
   } finally {
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -4,19 +4,41 @@ function circulationRepo() {
   const url = "mongodb://localhost:27017";
   const dbName = "circulation";
 
-  function get() {
+  function get(query, limit) {
     return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url);
       try {
         await client.connect();
         const db = client.db(dbName);
 
-        const items = await db.collection('newspapers').find();
+        let items = await db.collection('newspapers').find(query);
+        // collection.find({}).project({ a: 1 })                             // Create a projection of field a
+        // collection.find({}).skip(1).limit(10)                          // Skip 1 and limit 10
+        // collection.find({}).batchSize(5)                               // Set batchSize on cursor to 5
+        // collection.find({}).filter({ a: 1 })                              // Set query on the cursor
+        // collection.find({}).comment('add a comment')                   // Add a comment to the query, allowing to correlate queries
+        // collection.find({}).addCursorFlag('tailable', true)            // Set cursor as tailable
+        // collection.find({}).addCursorFlag('oplogReplay', true)         // Set cursor as oplogReplay
+        // collection.find({}).addCursorFlag('noCursorTimeout', true)     // Set cursor as noCursorTimeout
+        // collection.find({}).addCursorFlag('awaitData', true)           // Set cursor as awaitData
+        // collection.find({}).addCursorFlag('exhaust', true)             // Set cursor as exhaust
+        // collection.find({}).addCursorFlag('partial', true)             // Set cursor as partial
+        // collection.find({}).addQueryModifier('$orderby', { a: 1 })        // Set $orderby {a:1}
+        // collection.find({}).max(10)                                    // Set the cursor max
+        // collection.find({}).maxTimeMS(1000)                            // Set the cursor maxTimeMS
+        // collection.find({}).min(100)                                   // Set the cursor min
+        // collection.find({}).returnKey(10)                              // Set the cursor returnKey
+        // collection.find({}).setReadPreference(ReadPreference.PRIMARY)  // Set the cursor readPreference
+        // collection.find({}).showRecordId(true)                         // Set the cursor showRecordId
+        // collection.find({}).sort([['a', 1]])                           // Sets the sort order of the cursor query
+        // collection.find({}).hint('a_1')                                // Set the cursor hint
+        if (limit > 0) {
+          items = items.limit(limit);
+        }
 
         resolve(await items.toArray());
         client.close();
-      }
-      catch {
+      } catch (error) {
         reject(error);
       }
     })
